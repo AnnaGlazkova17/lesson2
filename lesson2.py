4e248ab7a24d9aff885bc1f259abaca9529e34d1
AnnaGlazkova17 <glazkova1706@gmail.com>
2024-09-16 23:09:36 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+# lesson2
+kol_vipol_dz = 12
+kol_chas = 1.5
+course = 'Python'
+sred_vremy = kol_chas/kol_vipol_dz
+print('Курс:',course,", всего задач: ",kol_vipol_dz,", затрачено часов: ", kol_chas, ', среднее время выполнения', sred_vremy, 'часа.' )
+
